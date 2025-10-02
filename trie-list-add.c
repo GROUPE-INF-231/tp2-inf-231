07f9f0ed48e5102c6da698104d56bfe449eb4013
Fagna Noel <fnoelvanys@gmail.com>
2025-10-02 03:08:50 +0100

    update trie-list-add.c with additional functionalities

--- trie-list-add.c
+++ trie-list-add.c
@@ -2,84 +2,144 @@
 #include <stdlib.h>
 
 
-struct list{
-	int val;
-	struct list* next;
-};
-typedef struct list list;
-typedef list* List;
+typedef struct linkedlist {
+	int data;
+	struct linkedlist* next;
+} list;
 
-List insert_trie(List L,int x){
-	List p=L;
-	List pd=(List)malloc(sizeof(list));
-	if(pd==NULL){
-		perror("->");
+// Using Insertion sort
+void sortingList(list** head) {
+	list* sorted = NULL;
+	list* current = *head;
+
+	while (current != NULL) {
+		list* nextNode = current->next;
+
+		if (sorted == NULL || current->data < sorted->data) {
+			current->next = sorted;
+			sorted = current;
+		}
+		else {
+			list* temp = sorted;
+
+			while (temp->next != NULL && temp->next->data <= current->data) {
+				temp = temp->next;
+			}
+			current->next = temp->next;
+			temp->next = current;
+		}
+
+		current = nextNode;
+	}
+
+	*head = sorted;
+}
+
+void insertIntoList(list** head, int elm) {
+	list* newNode = (list*)malloc(sizeof(list));
+	if (newNode == NULL) {
+		printf("Memory allocation failed!\n");
 		exit(1);
 	}
-	pd->val=x;
-	pd->next=NULL;
 
-	if(L==NULL ||  x<L->val){
-		pd->next=L;
-		return pd;
+	newNode->data = elm;
+	newNode->next = NULL;
+
+	if (*head == NULL || elm < (*head)->data) {
+		newNode->next = *head;
+		*head = newNode;
+		return;
 	}
-	while(p->next!=NULL && p->next->val<x){
-		p=p->next;
+
+	list* current = *head;
+	while (current->next != NULL && current->next->data < elm) {
+		current = current->next;
 	}
 
-	
-		pd->next=p->next;
-		p->next=pd;
-
-	
-	
-return L;
-
+	newNode->next = current->next;
+	current->next = newNode;
 }
 
-
-
-
-
-void afficherListe(List L){
-	List p=L;
-
-	if(p==NULL)
-		printf("il y'a aucun element dans la liste\n");
-
-	printf("Liste:");
-	while(p!=NULL){
-		printf("%d ->",p->val);
-		p=p->next;
+void printList(list* head) {
+	if (head == NULL) {
+		printf("List is empty!\n");
+		return;
 	}
 
-
-
+	printf("Liste: ");
+	while (head != NULL) {
+		printf("%d -> ", head->data);
+		head = head->next;
+	}
+	printf("NULL\n");
 }
 
-int main(){
-	List L=NULL;
-	int n,x;
-	printf("entrer le nombre d'element a inserer:\n");
-	scanf("%d",&n);
-	printf("entrer le nombre d'element pas ordre croissant\n");
-	for(int i=0;i<n;i++){
-		scanf("%d",&x);
-		L=insert_trie(L,x);
+list* createlist(int data) {
+	list* newNode = (list*)malloc(sizeof(list));
+	if (!newNode) {
+		printf("Memory allocation failed!\n");
+		exit(1);
+	}
+	newNode->data = data;
+	newNode->next = NULL;
+	return newNode;
+}
+
+void insertEnd(list** head, int data) {
+	list* newNode = createlist(data);
+
+	if (*head == NULL) {
+		*head = newNode;
+		return;
 	}
 
-	afficherListe(L);
+	list* temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = newNode;
+}
 
-	L=insert_trie(L,3);
+void freeList(list** head) {
+	list* current = *head;
+	list* nextNode;
 
-	afficherListe(L);
+	while (current != NULL) {
+		nextNode = current->next;
+		free(current);
+		current = nextNode;
+	}
+}
 
+int main() {
+	list* head = NULL;
+	int i, nb, elm, ins;
 
+	printf("Entrez le nombre d'element que vous voulez inserer: ");
+	do {
+		scanf("%d", &nb);
+		if (nb <= 0) {
+			printf("Erreur! Entrez une valeur valide.\n");
+		}
+	} while (nb <= 0);
 
-		free(L);
-	
-	
+	for (i = 0; i < nb; i++) {
+		printf("Valeur %d: ", i + 1);
+		scanf("%d", &elm);
+		insertEnd(&head, elm);
+	}
+	printList(head);
 
+	sortingList(&head);
+	printf("Liste triee en ordre croissant:\n");
+	printList(head);
+
+	printf("Entrez la valeur a ajoute de la liste: ");
+	scanf("%d", &ins);
+
+	insertIntoList(&head, ins);
+	printList(head);
+
+	freeList(&head);
 	return 0;
 }
 
